c02c419291d537a5cd582ffee85e44de7f9fb572
Waffles <mybowlisfullofcatfood@gmail.com>
2022-10-20 18:06:42 -0500

    bad remix app

--- 04 - Solidity Basics /VolcanoCoin.sol
+++ 04 - Solidity Basics /VolcanoCoin.sol
@@ -2,11 +2,11 @@
 
 pragma solidity ^0.8.0;
 
-contract VolcanoCoin {
-
+contract VolcanoCoin 
+{
     address immutable owner;
 
-    uint256 totalSupply = 10000; // Just read a 2016 post stating that unit8 costs more gas than unit256 ...does this still hold true? I would assume that applies to anything smaller than 256? 
+    uint256 totalSupply = 10000; // Just read a 2016 post stating that uint8 costs more gas than uint256 ...does this still hold true? I would assume that applies to 16, 32, etc? 
     
     struct Payment {
         uint256 amount;
@@ -17,14 +17,13 @@ contract VolcanoCoin {
     mapping(address => Payment[]) public paymentsByAddress;
     
     event IncrementTotalSupply(uint256 newTotalSupply);
-    event TransferExecuted(uint256 amount, address recipient);
+    event Sent(address from, address to, uint256 amount);
 
     error InsufficientBalance(uint256 requested, uint256 available);
 
     modifier onlyOwner {
-        if (msg.sender == owner) {
+        if (msg.sender == owner)
             _;
-        }
     }
 
     constructor() {
@@ -38,8 +37,7 @@ contract VolcanoCoin {
 
     // shouldn't the increment amount be a function param?
     // A function hardcoded to increment by 1000 seems silly
-    function incrementTotalSupplyBy(uint256 amnt) public onlyOwner returns (uint256 _totalSupply) 
-    {
+    function incrementTotalSupplyBy(uint256 amnt) public onlyOwner returns (uint256 _totalSupply) {
         uint256 newSupply = totalSupply + amnt;
         totalSupply = newSupply;
         
@@ -49,28 +47,23 @@ contract VolcanoCoin {
         return newSupply;
     }
 
-    function incrementTotalSupplyByThousand() public onlyOwner 
-    {
+    function incrementTotalSupplyByThousand() public onlyOwner {
         incrementTotalSupplyBy(1000);
     }
 
-    function transferAmountToAddress(uint256 amnt, address recipient) public 
-    {
-        if (balanceByAddress[msg.sender] > amnt) 
-        {
-            balanceByAddress[msg.sender] -= amnt;
-            balanceByAddress[recipient] += amnt;
-
-            // record senders transaction history
-            paymentsByAddress[msg.sender].push(Payment({amount: amnt, recipient: recipient}));
-            
-            emit TransferExecuted(amnt, recipient);
-        } else {
-            // Oops, you're too poor
+    function transferAmountToAddress(uint256 amnt, address recipient) public {
+        if (amnt > balanceByAddress[msg.sender]) // Oops, you're too poor
             revert InsufficientBalance({
                 requested: amnt, 
                 available: balanceByAddress[msg.sender]
                 });
-        }
+
+        balanceByAddress[msg.sender] -= amnt;
+        balanceByAddress[recipient] += amnt;
+
+        // record senders transaction history
+        paymentsByAddress[msg.sender].push(Payment({amount: amnt, recipient: recipient}));
+        
+        emit Sent(msg.sender, recipient, amnt);
     }
 }
